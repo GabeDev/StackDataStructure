68796c3afccc82b7165ff1f2f6da9f33ad8cdba0
GabeDev <gabriel.d.albu@gmail.com>
2022-03-31 11:34:30 -0400

    complete stack example

--- StackDataStructure.cpp
+++ StackDataStructure.cpp
@@ -1,6 +1,39 @@
 #include <iostream>
+#include <stack>
 //  uses LIFO Last In First Out
+using namespace std;
+
+//uses top
+void printStack(stack <int> input)
+{
+    while (!input.empty())
+    {
+        cout<<input.top()<<endl;
+        input.pop();
+    }
+        
+}
 int main()
 {
-    std::cout << "Hello World!\n";
+    stack <int> numbersStack;
+    //common fuctions
+    //push,pop,empty,size,top
+    
+    //push
+    numbersStack.push(1);
+    numbersStack.push(2);
+    numbersStack.push(3);
+    //pop
+    numbersStack.pop();
+
+  /*  if (numbersStack.empty())// use of empty
+    {
+        cout << "Stack is empty" << endl;
+        cout << "Stack size is: "<<numbersStack.size() << endl;//use of size
+    }
+    else
+    {
+        cout << "Stack is not empty";
+    }*/
+    printStack(numbersStack);
 }
